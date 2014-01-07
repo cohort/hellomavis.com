f5ff5ed3ead3ea9443d5441f325ac7fa73b4484b
Robert Sargent <hello@robertsargent.co.uk>
2014-01-07 11:35:06 +0000

    Had to edit grid-float-breakpoint variable in the base theme.

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6c39f42987ae297a5a21e2bb35bf3402');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f932286ed4019d3b6cc5109faf74c5df');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
