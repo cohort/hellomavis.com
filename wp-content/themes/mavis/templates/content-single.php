35a4a0e0c2c1cfe67372dcc392bf292b311fc540
Robert Sargent <hello@robertsargent.co.uk>
2014-01-11 08:47:19 +0000

    Single content page layout fixes/

--- wp-content/themes/mavis/templates/content-single.php
+++ wp-content/themes/mavis/templates/content-single.php
@@ -9,7 +9,18 @@
 		<h1 class="entry-title"><?php the_title( ) ; ?></h1>
 	</header>
 	<div class="entry-content">
-		<?php the_content( ) ; ?>
+		<?php if ( has_post_thumbnail( ) ) : ?>
+			<div class="row">
+				<div class="portrait col-md-4 col-sm-12">
+					<?php the_post_thumbnail( 'full' ) ; ?>
+				</div>
+				<div class="col-md-8 col-sm-12">
+					<?php the_content( ) ; ?>
+				</div>
+			</div>
+		<?php else : ?>
+			<?php the_content( ) ; ?>
+		<?php endif ; ?>
 	</div>
 </article>
 <?php endwhile; ?>
